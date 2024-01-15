318ef07124de39830f8588ba54bf3cae77b0132e
Abdul Kabeer <Abdul.Kabeer245@gmail.com>
2024-01-16 04:52:28 +0500

    Refactor code for disease identification, crop recommendation, and plant information page.

--- app.py
+++ app.py
@@ -2,6 +2,33 @@ from flask import Flask, render_template, request
 from prediction import load_and_classify, recommendation
 from flask_mysqldb import MySQL
 
+app = Flask('_name_', template_folder = 'Front-end', static_folder='Front-end', static_url_path='/Front-end/Assets')
+
+#database
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'grow_wise'
+app.config['MYSQL_PASSWORD'] = 'Test.12345'
+app.config['MYSQL_DB'] = 'grow_wise_db'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+
+models_list={
+    'apple':'apple_model',
+    'banana': 'banana_model',
+    'cherry': 'cherry_model',
+    'cotton': 'cotton_model',
+    'grape':'grape_model',
+    'mango': 'mango_model',
+    'maize': 'maize_model',
+    'peach': 'peach_model',
+    'potato':'potato_model',
+    'pepper': 'pepper_model',
+    'rice': 'rice_model',
+    'soybean': 'soybean_model',
+    'strawberry': 'strawberry_model',
+    'tomato':'tomato_model',
+    'wheat':'wheat_model'
+}
 
 
 @app.route('/')
@@ -24,71 +51,57 @@ def disease_identification():
                 cursor.execute(query)
                 identification_data = cursor.fetchone()
         return render_template('identification result.html', identification_data = identification_data, result = class_label)
-
     return render_template('disease_identification.html')
+
+
 #Crop Recommender page
-@app.route('/crop_recommender')
+@app.route('/crop_recommender', methods=['POST', 'GET'])
 def crop_recommender():
-    return render_template('crop_recommender.html')
-#about us page
-@app.route('/aboutus')
-def aboutus():
-    return render_template('aboutus.html')
-#faqs
-@app.route('/faqs')
-def faqs():
-    return render_template('faqs.html')
-#database
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'grow_wise'
-app.config['MYSQL_PASSWORD'] = 'Test.12345'
-app.config['MYSQL_DB'] = 'grow_wise_db'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-mysql = MySQL(app)
-#Recommender part
-@app.route('/predict', methods=['POST', 'GET'])
-def predict():
-    if request.method == 'POST':
-        cur = mysql.connection.cursor()
-        N = float(request.form['nitrogen'])
-        P = float(request.form['phosphorous'])
-        K = float(request.form['k'])
-        temperature = float(request.form['temperature'])
-        humidity = float(request.form['Humidity'])
-        PH = float(request.form['PH'])
-        rain_fall = float(request.form['rain_fall'])
+        if request.method == 'POST':
+            cur = mysql.connection.cursor()
+            N = float(request.form['nitrogen'])
+            P = float(request.form['phosphorous'])
+            K = float(request.form['k'])
+            temperature = float(request.form['temperature'])
+            humidity = float(request.form['Humidity'])
+            PH = float(request.form['PH'])
+            rain_fall = float(request.form['rain_fall'])
 
-        output = recommendation(N, P, K, temperature, humidity, PH, rain_fall)
+            output = recommendation(N, P, K, temperature, humidity, PH, rain_fall)
 
-        # Insert data into the table
-        insert_query = "INSERT INTO user_input_data (N, P, K, temperature, humidity, ph, rainfall, crop) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-        cur.execute(insert_query, (N, P, K, temperature, humidity, PH, rain_fall, output))
-        mysql.connection.commit()
+            # Insert data into the table
+            insert_query = "INSERT INTO user_input_data (N, P, K, temperature, humidity, ph, rainfall, crop) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+            cur.execute(insert_query, (N, P, K, temperature, humidity, PH, rain_fall, output))
+            mysql.connection.commit()
 
-        # Fetch data from the CropTable1
-        select_query = "SELECT * from plantinformation WHERE name = %s"
-        cur.execute(select_query, (output, ))
-        crop_data = cur.fetchone()
+            # Fetch data from the CropTable1
+            select_query = "SELECT * from plantinformation WHERE name = %s"
+            cur.execute(select_query, (output, ))
+            crop_data = cur.fetchone()
 
-        # Close the cursor
-        cur.close()
+            # Close the cursor
+            cur.close()
 
-        # Render the output page with the output
-        print("Output Value:", output)
-        return render_template('recommend_output.html', output=output, crop_data=crop_data)
-    else:
-        return render_template('crop_recomender.html')
-    #plantInfo
+            # Render the output page with the output
+            print("Output Value:", output)
+            return render_template('recommend_output.html', output=output, crop_data=crop_data)
+        else:
+            return render_template('crop_recomender.html')
+        return render_template('crop_recommender.html')
+
+
+
+
 @app.route('/plant_information', methods=['GET', 'POST'])
 def plant_information():
     if request.method == 'POST':
         # Acquiring the query from search bar
-        nam = request.form['searchInput']
+        query = request.form['searchInput']
         cur = mysql.connection.cursor()
         
         # Searching for the relevent data
         select_query = "SELECT * from PlantInformation WHERE name = %s"
-        cur.execute(select_query, (nam, ))
+        cur.execute(select_query, (query, ))
         data = cur.fetchone()
 
         cur.close()
--- prediction.py
+++ prediction.py
@@ -1,11 +1,58 @@
-import flask
+from tensorflow.keras.models import load_model
+from PIL import Image
 import numpy as np
+import os
 import sklearn
 from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 from tensorflow import keras
 import pickle
-import os
+
+def predicting_class(model_name):
+    models_category = {
+    'apple_model': ['Scab', 'Black Rot', 'Cedar', 'Healthy'],
+    'banana_model': ['Cordana Leaf Spot', 'Healthy', 'Pestalotiopsis', 'Sigatoka'],
+    'cherry_model': ['Healthy', 'Powdery Mildew'],
+    'cotton_model': ['Jassid', 'Mealybug', 'Aphid', 'Armyworm', 'Healthy', 'Thrips'],
+    'grape_model':['Black root','Esca','Leaf Blight','Healthy'],
+    'mango_model': ['Anthracnose','Becterial Canker', 'Cutting Weevil','Die Back','Gall Midge','Healthy','Powdery Mildew', 'Sooty Mould'],
+    'maize_model': ['Gray Leaf spot','Common Rust','Northern Leaf Blight','Healthy'],
+    'peach_model':['Bacterial Spot','Healthy'],
+    'potato_model':['Early Blight','Late Blight','Healthy'],
+    'pepper_model':['Healthy','Black Spot'],
+    'rice_model':['Brown Spot','Healthy','Hispa','Leaf Blast'],
+    'soybean_model':['Bacterial Pustule','Frogeye Leaf Spot','Healthy','Rust','Sudden Death Syndrome','Target Leaf Spot','Yellow Mosaic'],
+    'strawberry_model':['Healthy','Leaf Scorch'],
+    'tomato_model':['Bacterial Scab','Early Blight','Late Blight','Leaf Mould','Septoria Leaf Spot','Spider Mites','Target Spot','Yellow Leaf Curl Virus','Mosaic Virus','Healthy'],
+    'wheat_model':['Brown Rust','Healthy','Septoria','Strip Rust']
+    }
+    categories = models_category.get(model_name)
+    return categories
+
+def load_and_classify(model_name, image_path):
+    script_dir = os.path.dirname(__file__)
+
+    # Navigate to the 'models' folder using a relative path
+    models_folder = os.path.join(os.pardir, 'model')
+    model_path = os.path.join(models_folder, f'{model_name}.h5')
+    model = load_model(model_path)
+    uploaded_image = Image.open(image_path)
+    uploaded_image = uploaded_image.resize((224, 224))
+    # Preprocess the image
+    uploaded_image = np.array(uploaded_image) / 255.0  # Normalize pixel values
+    uploaded_image = np.expand_dims(uploaded_image, axis=0)  # Expand dimensions for batch processing
+
+    # Make the prediction
+    prediction = model.predict(uploaded_image)
+
+    # Get the predicted class
+    predicted_class = np.argmax(prediction)
+    class_list = predicting_class(model_name)
+    class_label = class_list[predicted_class]
+    return class_label
+
+
+
 
     # Load the saved deep learning model
 loaded_model="../models/crop_rec_model.h5"
@@ -57,51 +104,4 @@ def recommendation(N, P, K, temperature, humidity, ph, rainfall):
     
     crop_id = np.argmax(prediction)
     recommended_crop = crop_names[crop_id]
-    return rfrom tensorflow.keras.models import load_model
-from PIL import Image
-import numpy as np
-import os
-
-def predicting_class(model_name):
-    models_category = {
-    'apple_model': ['Scab', 'Black Rot', 'Cedar', 'Healthy'],
-    'banana_model': ['Cordana Leaf Spot', 'Healthy', 'Pestalotiopsis', 'Sigatoka'],
-    'cherry_model': ['Healthy', 'Powdery Mildew'],
-    'cotton_model': ['Jassid', 'Mealybug', 'Aphid', 'Armyworm', 'Healthy', 'Thrips'],
-    'grape_model':['Black root','Esca','Leaf Blight','Healthy'],
-    'mango_model': ['Anthracnose','Becterial Canker', 'Cutting Weevil','Die Back','Gall Midge','Healthy','Powdery Mildew', 'Sooty Mould'],
-    'maize_model': ['Gray Leaf spot','Common Rust','Northern Leaf Blight','Healthy'],
-    'peach_model':['Bacterial Spot','Healthy'],
-    'potato_model':['Early Blight','Late Blight','Healthy'],
-    'pepper_model':['Healthy','Black Spot'],
-    'rice_model':['Brown Spot','Healthy','Hispa','Leaf Blast'],
-    'soybean_model':['Bacterial Pustule','Frogeye Leaf Spot','Healthy','Rust','Sudden Death Syndrome','Target Leaf Spot','Yellow Mosaic'],
-    'strawberry_model':['Healthy','Leaf Scorch'],
-    'tomato_model':['Bacterial Scab','Early Blight','Late Blight','Leaf Mould','Septoria Leaf Spot','Spider Mites','Target Spot','Yellow Leaf Curl Virus','Mosaic Virus','Healthy'],
-    'wheat_model':['Brown Rust','Healthy','Septoria','Strip Rust']
-    }
-    categories = models_category.get(model_name)
-    return categories
-
-def load_and_classify(model_name, image_path):
-    script_dir = os.path.dirname(__file__)
-
-    # Navigate to the 'models' folder using a relative path
-    models_folder = os.path.join(os.pardir, 'model')
-    model_path = os.path.join(models_folder, f'{model_name}.h5')
-    model = load_model(model_path)
-    uploaded_image = Image.open(image_path)
-    uploaded_image = uploaded_image.resize((224, 224))
-    # Preprocess the image
-    uploaded_image = np.array(uploaded_image) / 255.0  # Normalize pixel values
-    uploaded_image = np.expand_dims(uploaded_image, axis=0)  # Expand dimensions for batch processing
-
-    # Make the prediction
-    prediction = model.predict(uploaded_image)
-
-    # Get the predicted class
-    predicted_class = np.argmax(prediction)
-    class_list = predicting_class(model_name)
-    class_label = class_list[predicted_class]
-    return class_label
-    
+    return recommended_crop
