846aa0f45e849d90c31df92ce78abd93638c0950
Abdul Kabeer <Abdul.Kabeer245@gmail.com>
2024-01-30 00:35:50 +0500

    Code updated subtle changes made.

--- prediction.py
+++ prediction.py
@@ -4,62 +4,9 @@ import numpy as np
 import os
 import sklearn
 from sklearn.preprocessing import MinMaxScaler
-<<<<<<< HEAD
-from tensorflow.keras.layers import Layer
-
-# from tensorflow import keras
 import pickle
 
-def predicting_class(model_name):
-    models_category = {
-    'apple_model': ['Scab', 'Black Rot', 'Cedar Rust', 'Healthy'],
-    'banana_model': ['Cordana Leaf Spot', 'Healthy', 'Pestalotiopsis', 'Sigatoka'],
-    'cherry_model': ['Powdery Mildew', 'Healthy'],
-    'cotton_model': ['Jassid', 'Mealybug', 'Aphid', 'Armyworm', 'Healthy', 'Thrips'],
-    'grape_model':['Black rot','Esca','Leaf Blight','Leaf Spot','Healthy'],
-    'mango_model':['Anthracnose','Becterial Canker','Cutting Weevil','Die Back','Gall Midge','Healthy','Powdery Mildew','Sooty Mould'],
-    'maize_model':['Gray Leaf spot','Common Rust','Northern Leaf Blight','Healthy'],
-    'peach_model':['Bacterial Spot','Healthy'],
-    'potato_model':['Early Blight','Late Blight','Healthy'],
-    'pepper_model':['Black Spot','Healthy'],
-    'rice_model':['Brown Spot','Healthy','Hispa','Leaf Blast'],
-    'soybean_model':['Bacterial Pustule','Frogeye Leaf Spot','Healthy','Rust','Sudden Death Syndrome','Target Leaf Spot','Yellow Mosaic'],
-    'strawberry_model':['Healthy','Leaf Scorch'],
-    'tomato_model':['Bacterial Scab','Early Blight','Late Blight','Leaf Mould','Septoria Leaf Spot','Spider Mites','Target Spot','Yellow Leaf Curl Virus','Mosaic Virus','Healthy'],
-    'wheat_model':['Brown Rust','Healthy','Septoria','Strip Rust']
-    }
-    categories = models_category.get(model_name)
-    return categories
-
-def load_and_classify(model_name, image_path):
-    script_dir = os.path.dirname(__file__)
-
-    # Navigate to the 'models' folder using a relative path
-    models_folder = os.path.join(os.pardir, 'model')
-    model_path = os.path.join(models_folder, f'{model_name}.h5')
-    model = load_model(model_path)
-    uploaded_image = Image.open(image_path)
-    uploaded_image = uploaded_image.resize((224, 224))
-    uploaded_image = uploaded_image.convert('RGB') # convert the image to RGB
-    # Preprocess the image
-    uploaded_image = np.array(uploaded_image) / 255.0  # Normalize pixel values
-    uploaded_image = np.expand_dims(uploaded_image, axis=0)  # Expand dimensions for batch processing
-
-    # Make the prediction
-    prediction = model.predict(uploaded_image)
-
-    # Get the predicted class
-    predicted_class = np.argmax(prediction)
-    class_list = predicting_class(model_name)
-    class_label = class_list[predicted_class]
-    return class_label
-
-# Recommender part
-
-=======
-# from tensorflow import keras
-import pickle
-
+#  Predicting class labels for disease indentification model
 def predicting_class(model_name):
     models_category = {
     'apple_model': ['Scab', 'Black Rot', 'Cedar Rust', 'Healthy'],
@@ -81,9 +28,9 @@ def predicting_class(model_name):
     categories = models_category.get(model_name)
     return categories
 
+#  Loading the trained model and pre-process the input image for disease identification
 def load_and_classify(model_name, image_path):
     script_dir = os.path.dirname(__file__)
-
     # Navigate to the 'models' folder using a relative path
     models_folder = os.path.join(os.pardir, 'model')
     model_path = os.path.join(models_folder, f'{model_name}.h5')
@@ -105,8 +52,6 @@ def load_and_classify(model_name, image_path):
     return class_label
 
 # Recommender part
-
->>>>>>> 4199c3d1252f90ed20901673469675ac452755e7
 def recommendation(N, P, K, temperature, humidity, ph, rainfall):
     # Load the saved deep learning model
     loaded_model_path = "../model/crop_rec_model.h5"
